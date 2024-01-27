8003e9336e9ec085c6a7b5f61fe1d958c2553bdf
Spencer Presley <spencerpresley96@gmail.com>
2024-01-27 12:18:44 -0500

    Updated webscrapper.py to call data_upload()
    updated datauploader.py to verify page metadata isn't too large
    
    implemented validate_metadata_size()function to verify metadata size
    
    for the validation, if the metadata is too large stores it into dict
    
    for dict, key=blob_name, value=metadata associated with the blob_name

--- Tools/datauploader.py
+++ Tools/datauploader.py
@@ -1,6 +1,7 @@
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 import os
 from dotenv import load_dotenv
+import pickle
 
 load_dotenv()
 
@@ -10,19 +11,48 @@ class BlobTools:
     Class to check if a blob exists, if it doesn't create it and upload data
     """
     def __init__(self):
+        # evironment variables
         self.connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
         self.container_name = os.getenv("AZURE_STORAGE_CONTAINER_NAME")
         self.blob_service_client = BlobServiceClient.from_connection_string(self.connection_string)
         self.container_client = self.blob_service_client.get_container_client(self.container_name)
         
-    def data_upload(self, page_name, page_content, page_meta_data):
+        # dictionary to keep track of any pages whos metadata is too large
+        # key = page_name, value = page_metadata 
+        self.oversized_metadata = {}
+                
+    def validate_metadata_size(self, blob_name, page_metadata)->bool:
+        """
+            Args:
+                page_name: name of page
+                page_metadata: metadata for the page associated with page_name
+            Returns:
+                True if the metadata is within the size limits, False if it is too large
+            If the metadata is too large, it is added to the oversized_metadata dictionary
+            The blob_name is added as the key and the page_metadata as the value
+            blob_name is the page's string contents in bytes in the form of a .txt file
+        """   # get size of the pages metadata
+        size_of_metadata = len(pickle.dumps(page_metadata))
+        
+        # if it's within the size limits (8kb) just return
+        if size_of_metadata <= 8192:
+            return True
+        
+        # if the meta data is too large add that page and it's metadata to oversized_metadata
+        self.oversized_metadata[blob_name] = page_metadata
+        return False
+        # raise error if metadata is too large
+        # raise ValueError("Metadata size is too large, adding to dictionary and continuing")
+
+    
+    def data_upload(self, page_name, page_content, page_metadata):
         """
             Uploads data to Azure Blob Storage. If the blob does not exist, it is created as an append blob.
             Then, school_data is appended to the blob
 
             :param school_name: The name of the school, used to create blob name.
             :param school_data: Data to be uploaded
-        """  
+        """
         blob_name = f"{page_name}.txt"
         blob_client = self.container_client.get_blob_client(blob=blob_name)
         
@@ -33,6 +63,13 @@ class BlobTools:
             
         # append data to blob
         blob_client.append_block(page_data_bytes)
-        print(f"Data appended to blob: {blob_name}")
+        
+        # validate metadata is within size limits, if it is upload it, if it isn't skip it
+        if self.validate_metadata_size(blob_name, page_metadata):
+            blob_client.set_blob_metadata(metadata=page_metadata)
+            print(f"page_content and page_metadata for {page_name} were appended to blob: {blob_name}")
+            return
+        print(f"Page content for {page_name} was appended to blob but page_metadata was too large and wasn't appended")
+        return
     
     
--- Tools/webscrapper.py
+++ Tools/webscrapper.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup as Soup
 from langchain_community.document_loaders.recursive_url_loader import RecursiveUrlLoader
-#from datauploader import BlobTools
-#blob_tools = BlobTools()
+from datauploader import BlobTools
+blob_tools = BlobTools()
 university_info = {
     "https://www.salisbury.edu/": "salisbury_university_page"
     }
@@ -16,12 +16,13 @@ for i, page in enumerate(pages):
     # actual content on page
     page_content = page.page_content
     # metadata about page, url, title, description, etc.
-    page_meta_data = page.metadata
+    page_metadata = page.metadata
+    blob_tools.validate_metadata_size(page_metadata)
     # getting the url associated with the page
-    url = page_meta_data['source']
+    url = page_metadata['source']
     # rename the page to {university_name_(i+1)}, i.e. 'salisbury_university_page_1'
     blob_name = f"{university_info[url]}_{i+1}"
     # upload the doc to the blob container
-    # blob_tools.data_upload(blob_name, page_content, page_meta_data)
+    blob_tools.data_upload(blob_name, page_content, page_metadata)
 print(pages[0].metadata)
 
