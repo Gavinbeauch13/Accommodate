4a2d1f4bf616788f50513426cd50b7df8b86c9d3
Spencer Presley <spencerpresley96@gmail.com>
2024-01-27 03:01:00 -0500

    Slight changes to BlobTools method data_upload

--- Tools/datauploader.py
+++ Tools/datauploader.py
@@ -14,8 +14,7 @@ class BlobTools:
         self.blob_service_client = BlobServiceClient.from_connection_string(self.connection_string)
         self.container_client = self.blob_service_client.get_container_client(self.container_name)
         
-    @staticmethod
-    def data_upload(self, school_name, school_data):
+    def data_upload(self, page_name, page_data):
         """
             Uploads data to Azure Blob Storage. If the blob does not exist, it is created as an append blob.
             Then, school_data is appended to the blob
@@ -23,15 +22,16 @@ class BlobTools:
             :param school_name: The name of the school, used to create blob name.
             :param school_data: Data to be uploaded
         """  
-        blob_name = f"{school_name}_data"
+        blob_name = f"{page_name}.txt"
         blob_client = self.container_client.get_blob_client(blob=blob_name)
         
+        page_data_bytes = page_data.encode('utf-8')
         # Check if the blob exists, if it doesn't create it
         if not blob_client.exists():
             blob_client.create_append_blob()
             
         # append data to blob
-        blob_client.append_block(school_data)
+        blob_client.append_block(page_data_bytes)
         print(f"Data appended to blob: {blob_name}")
         
     
