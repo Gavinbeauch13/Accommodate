25b5d0b9d5d4cad1ebfd0ac5cd52cc0ace360cec
Spencer Presley <spencerpresley96@gmail.com>
2024-01-27 09:57:00 -0500

    updates

--- Tools/datauploader.py
+++ Tools/datauploader.py
@@ -1,4 +1,4 @@
-from azure.storage.blob import BlobServiceClient
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 import os
 from dotenv import load_dotenv
 
@@ -14,7 +14,7 @@ class BlobTools:
         self.blob_service_client = BlobServiceClient.from_connection_string(self.connection_string)
         self.container_client = self.blob_service_client.get_container_client(self.container_name)
         
-    def data_upload(self, page_name, page_data):
+    def data_upload(self, page_name, page_content, page_meta_data):
         """
             Uploads data to Azure Blob Storage. If the blob does not exist, it is created as an append blob.
             Then, school_data is appended to the blob
@@ -25,7 +25,7 @@ class BlobTools:
         blob_name = f"{page_name}.txt"
         blob_client = self.container_client.get_blob_client(blob=blob_name)
         
-        page_data_bytes = page_data.encode('utf-8')
+        page_data_bytes = page_content.encode('utf-8')
         # Check if the blob exists, if it doesn't create it
         if not blob_client.exists():
             blob_client.create_append_blob()
